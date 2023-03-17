94f034fc302e636a5cb83cf2a85a7a6348673af2
adanmolina98 <128194185+adanmolina98@users.noreply.github.com>
2023-03-17 17:36:50 -0600

    Add files via upload

---
+++ src/NutritionistOffice.java
@@ -0,0 +1,42 @@
+public class NutritionistOffice {
+    public class Consultory {
+        private String ubication;
+        private String material;
+        private String doctor;
+        private String type;
+    }
+
+    public class Fruit {
+        private String name;
+        private double weight;
+        private double price;
+        private String colour;
+    }
+    import java.util.List;
+
+    public class Inventory {
+
+        private List<Guitar> guitars;
+    }
+    public class Jewel {
+        private String material;
+        private double weight;
+        private double carats;
+        private String form;
+        public class Main {
+            public static void main(String[] args) {
+                System.out.println("Hello world!");
+
+
+
+
+                int num1 = 2;
+
+                int num2 = 2;
+
+                int resultado = num1 + num2;
+
+                System.out.println("El resultado de la suma es: " + resultado);
+            }
+
+}
