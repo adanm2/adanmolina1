65b7f17bec09a2472025b3a71a0b448e6641b327
adanmolina98 <128194185+adanmolina98@users.noreply.github.com>
2023-03-17 21:53:23 -0600

    Add files via upload

---
+++ guitar.java
@@ -0,0 +1,29 @@
+public class guitar {
+    private String serialNumber;
+
+    private double price;
+
+
+    private String builder;
+
+    private String model;
+
+
+    private String type;
+
+    private String backWood;
+
+    private String topWood;
+
+}
+
+----------------------------------------------------------------------------------------------------------------
+
+        import java.util.List;
+
+public class Inventory {
+
+    private List<guitars> guitars;
+
+}
+}
